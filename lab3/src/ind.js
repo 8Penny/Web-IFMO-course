33a48cde50cf07ab5d67c376d0af9624f9cdb59e
8penny <iampolya@ya.ru>
2019-04-09 09:01:15 +0300

    load image

--- lab3/src/ind.js
+++ lab3/src/ind.js
@@ -1,4 +1,8 @@
-window.onload = () => {
+
+const COLLECTION_URL =
+  "https://source.unsplash.com/collection/1065412/";
+const images = []
+function addCanvas() {
   canvas = document.createElement("canvas");
   canvas.width = 500;
   canvas.height = 500;
@@ -10,11 +14,40 @@ window.onload = () => {
   ctx.textAlign = "center"
   ctx.filter = 'grayscale(35%)'
   
-  ctx.fillText("SIMPLE TEXT", 250, 50)
+  ctx.fillText("Loading...", 250, 50)
+}
+
+function addSaveButton() {
   saveButton = document.createElement("a");
   saveButton.download = "web3.jpg"
   saveButton.innerHTML = "Save image";
-  ctx.fillText("Another text", 250, 250)
+
   document.body.appendChild(saveButton);
+}
+
+function loadImage(width, height, left, top) {
+  const image = new Image();
+
+  image.crossOrigin = 'anonymous'
+
+  image.src = `${COLLECTION_URL}${width}x${height}`;
   
+  image.onload = () => {
+    images.push({ image, left, top })
+
+    imagesAreLoaded = 1
+    if (imagesAreLoaded ) {
+      ctx.drawImage(image, left, top)
+    }
+  };
+}
+
+
+
+
+window.onload = () => {
+  addCanvas();
+  addSaveButton();
+  loadImage(300, 200, 0, 0);
+
 }
