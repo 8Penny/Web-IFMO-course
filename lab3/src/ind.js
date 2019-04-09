8b41beb92638c57888f28a147047a9b5210dd872
8penny <iampolya@ya.ru>
2019-04-09 09:49:25 +0300

    generate collage

--- lab3/src/ind.js
+++ lab3/src/ind.js
@@ -1,7 +1,18 @@
 
 const COLLECTION_URL =
   "https://source.unsplash.com/collection/1065412/";
+
 const images = []
+let totalImagesCount
+
+let canvas;
+let ctx;
+let saveButton;
+let quoteText;
+let quoteIsLoaded = false;
+let imagesAreLoaded = false;
+let loadedImagesCount = 0;
+
 function addCanvas() {
   canvas = document.createElement("canvas");
   canvas.width = 500;
@@ -34,10 +45,12 @@ function loadImage(width, height, left, top) {
   
   image.onload = () => {
     images.push({ image, left, top })
+    loadedImagesCount++
 
-    imagesAreLoaded = 1
-    if (imagesAreLoaded ) {
-      ctx.drawImage(image, left, top)
+    imagesAreLoaded = loadedImagesCount === totalImagesCount
+    
+    if (imagesAreLoaded && quoteIsLoaded) {
+      displayContent()
     }
   };
 }
@@ -52,48 +65,72 @@ async function generateText() {
   quoteIsLoaded = true 
 
   if (imagesAreLoaded) {
-    ctx.filter = ''
-    const quoteChunks = []
-    let c = 0
-    let j = 0
-
-    for(let i = 0; i < quoteText.length; i++) {
-      c++
-      if (c > 20 && quoteText[i] === ' ') {
-        quoteChunks.push(quoteText.slice(j, i))
-        j = i
-        c = 0
-      }
-    }
-  
-    quoteChunks.push(quoteText.slice(j))
-    
-    console.log(quoteChunks, quoteText)
-
-    ctx.fillStyle = "#ffffff"
-
-    const chunksCount = quoteChunks.length
-
-    for (let i = 0; i< chunksCount; i++){
-
-      ctx.shadowColor="black";
-      ctx.shadowBlur=7;
-      ctx.lineWidth=3;
-      ctx.strokeText(quoteChunks[i], 250, 250 + (40* (i-chunksCount/2)))
-      ctx.shadowBlur=0;
-      ctx.fillStyle="white";
-
-      ctx.fillText(quoteChunks[i], 250, 250 + (40* (i-chunksCount/2)))
-    }
+    displayContent()
   }
 }
 
+
+function generateImageCollage() {
+
+  totalImagesCount = 4
+  loadImage(300, 200, 0, 0);
+  loadImage(200, 200, 300, 0);
+  loadImage(200, 300, 0, 200);
+  loadImage(300, 300, 200, 200);
+  
+}
+
+function displayContent() {
+  ctx.clearRect(0,0,500,500)
+  displayImages()
+  displayText()
+}
+
+function displayImages() {
+  images.forEach(({image,left,top})=>ctx.drawImage(image, left, top))
+}
+
+function displayText() {
+  ctx.filter = ''
+  const quoteChunks = []
+  let c = 0
+  let j = 0
+
+  for(let i = 0; i < quoteText.length; i++) {
+    c++
+    if (c > 20 && quoteText[i] === ' ') {
+      quoteChunks.push(quoteText.slice(j, i))
+      j = i
+      c = 0
+    }
+  }
+
+  quoteChunks.push(quoteText.slice(j))
+  
+  console.log(quoteChunks, quoteText)
+
+  ctx.fillStyle = "#ffffff"
+
+  const chunksCount = quoteChunks.length
+
+  for (let i = 0; i< chunksCount; i++){
+
+    ctx.shadowColor="black";
+    ctx.shadowBlur=7;
+    ctx.lineWidth=3;
+    ctx.strokeText(quoteChunks[i], 250, 250 + (40* (i-chunksCount/2)))
+    ctx.shadowBlur=0;
+    ctx.fillStyle="white";
+
+    ctx.fillText(quoteChunks[i], 250, 250 + (40* (i-chunksCount/2)))
+  }
+}
 
 window.onload = () => {
   addCanvas();
   addSaveButton();
   
-  loadImage(300, 200, 0, 0);
+  generateImageCollage();
   generateText();
 
 }
